7b21704985f8e58261bd4d3840fe4522ade32f4b
CamilingJS <jonathancamiling@gmail.com>
2023-01-22 16:13:14 -0800

    creating a cart for a user with no login credentials

--- routes/products.js
+++ routes/products.js
@@ -1,35 +1,12 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
-const multer = require('multer');
-
-const productsRepo = require('../../repositories/products');
-const productsNewTemplate = require('../../views/admin/products/new');
-const { requireTitle, requirePrice } = require('./validators');
+const productsRepo = require('../repositories/products');
+const productsIndexTemplate = require('../views/products/index');
 
 const router = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
-
-router.get('/admin/products/new', (req, res) => {
-  res.send(productsNewTemplate({}));
+router.get('/', async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
 });
-
-router.post(
-  '/admin/products/new',
-  [requireTitle, requirePrice],
-  upload.single('image'),
-  async (req, res) => {
-    const errors = validationResult(req);
-
-    const image = req.file.buffer.toString('base64');
-    const {title, price} = req.body;
-    await productsRepo.create({title, price, image})
-
-    console.log(req.file);
-
-    res.redirect('/admin/products');
-  }
-);
 
 module.exports = router;
